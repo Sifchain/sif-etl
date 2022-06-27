fd4aa00cd7c38f385511050daa13e4eb249460f0
Shawn Xiao <daoqing@sifchain.finance>
2022-06-28 05:21:25 +1200

    bug fix cursor already closed issue

--- src/services/database.py
+++ src/services/database.py
@@ -6,6 +6,11 @@ from src.services.config import config_service
 
 class DatabaseService:
     def __init__(self) -> None:
+        self.connection = self.get_conn()
+        self.cursor = self.get_cursor()
+        self.dict_cursor = self.get_dict_cursor()
+
+    def get_conn(self):
         self.connection = psycopg2.connect(
             database=config_service.pg_config['database'],
             user=config_service.pg_config['user'],
@@ -13,18 +18,36 @@ class DatabaseService:
             host=config_service.pg_config['host'],
             port=config_service.pg_config['port']
         )
+        return self.connection
+
+    def get_dict_cursor(self):
+        self.connection = self.get_conn()
         self.connection.autocommit = True
-        self.cursor = self.connection.cursor()
-        self.dict_cursor = self.connection.cursor(
+        return self.connection.cursor(
             cursor_factory=psycopg2.extras.DictCursor)
 
+    def get_cursor(self):
+        self.connection = self.get_conn()
+        self.connection.autocommit = True
+        return self.connection.cursor()
+
     def execute_scalar(self, sql_str):
-        self.cursor.execute(sql_str)
+        try:
+            self.cursor.execute(sql_str)
+        except Exception as e:
+            print(e)
+            self.cursor = self.get_cursor()
+            self.cursor.execute(sql_str)
         record = self.cursor.fetchall()[0][0]
         return record
 
     def execute_query(self, sql_str):
-        self.dict_cursor.execute(sql_str)
+        try:
+            self.dict_cursor.execute(sql_str)
+        except Exception as e:
+            print(e)
+            self.dict_cursor = self.get_dict_cursor()
+            self.dict_cursor.execute(sql_str)
         ans = self.dict_cursor.fetchall()
         dict_result = []
         for row in ans:
@@ -32,7 +55,12 @@ class DatabaseService:
         return dict_result
 
     def execute_update(self, sql_str):
-        self.cursor.execute(sql_str)
+        try:
+            self.cursor.execute(sql_str)
+        except psycopg2.InterfaceError as e:
+            print(e)
+            self.cursor = self.get_cursor()
+            self.cursor.execute(sql_str)
         self.connection.commit()
 
 
