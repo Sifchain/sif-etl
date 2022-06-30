6a545dbdd6bb11d484d419dcc80eb918aa507e57
Shawn Xiao <daoqing@sifchain.finance>
2022-07-01 10:32:38 +1200

    implimented multi-proc load

--- multiproc.py
+++ multiproc.py
@@ -44,15 +44,8 @@ class IngestWorker(Thread):
             start, end, unprocessed_heights = self.queue.get()
             logger.debug(f"start = {start}, end = {end}, unprocessed_heights={len(unprocessed_heights)}")
             try:
-                if self.mode == "reprocess":
-                    pass
-                    # multi_run_refresh(start, end, processed_heights)
-                elif self.mode in ["historical", "latest"]:
+                if self.mode in ["historical", "latest"]:
                     multi_run(start, end, unprocessed_heights)
-                # elif self.mode == "claims":
-                #     multi_run_claims(start, end, processed_heights)
-                # elif self.mode == "txn_detail":
-                #     process_event_txn(processed_heights)
                 else:
                     raise Exception("run mode not specified")
             finally:
@@ -62,63 +55,61 @@ class IngestWorker(Thread):
 def _process_events(num_threads, initial_start, num_events, mode, cached_height):
     queue = Queue()
     logger.info(f"Setting up the Queue - {mode} - {len(cached_height)}")
-
     for x in range(num_threads):
         worker = IngestWorker(queue, mode)
         worker.daemon = True
         worker.start()
-
     for x in range(num_threads):
         start = x * num_events + initial_start
         end = start + num_events - 1
         queue.put((start, end, cached_height))
-
     queue.join()
 
 
 def _process_latest_events():
-    latest_processed_height = 7074426 # get_latest_processed_height_query()
-    latest_height = get_latest_block_height_sifapi()
-    unprocessed_heights = get_unprocessed_heights_query(latest_height, latest_processed_height)
+    latest_processed_height = 7074426  # get_latest_processed_height_query()
+    last_event = get_latest_block_height_sifapi()
+    unprocessed_heights = get_unprocessed_heights_query(last_event, latest_processed_height)
 
-    num_threads = 10
-    num_events_per_thread = (int(latest_height - latest_processed_height) // num_threads + 1)
-    _process_events(num_threads,
-                    latest_processed_height,
-                    num_events_per_thread,
-                    "latest",
-                    unprocessed_heights,
-                    )
+    num_threads = 50
+    num_events_per_thread = (int(last_event - latest_processed_height) // num_threads + 1)
+    _process_events(num_threads, latest_processed_height, num_events_per_thread, "latest", unprocessed_heights, )
+
+
+def _process_historical():
+    last_event = get_latest_processed_height_query()
+    already_processed_block_heights = get_unprocessed_heights_query(last_event)
+    # Assume 100 events per minute per thread
+    # Gives 4 hours to process everything
+    num_events_per_thread = 100 * 60 * 4
+    num_threads = int(last_event // num_events_per_thread) + 1
+    _process_events(num_threads, 1, num_events_per_thread, "historical", already_processed_block_heights, )
+
+
+def _process_since_event(start_at, event):
+    last_event = get_latest_block_height_sifapi()
+    logger.info(f"last_event: {last_event}")
+    already_processed_block_heights = get_unprocessed_heights_query(last_event, start_at, event)
+    # Assume 100 events per minute per thread
+    # Gives 4 hours to process everything
+    num_events_per_thread = 100 * 60 * 4
+    num_threads = int(last_event // num_events_per_thread) + 1
+    _process_events(num_threads, 1, num_events_per_thread, "historical", already_processed_block_heights, )
 
 
 def main(mode=""):
-    # mode latest, historical, reprocess, txn_detail
+    # mode latest, historical, since, event
     ts = time()
     if mode == "historical":
-        pass
-    # elif mode == "since":
-    #     _process_since(5359241)
-    # elif mode == "refresh":
-    #     pass
-    # _process_already_processed_events_again()
+        _process_historical()
+    elif mode == "since":
+        _process_since_event(5359241, "swap_successful")
     elif mode == "latest":
         _process_latest_events()
-    # elif mode == "txn_detail":
-    #     _process_txn_detail()
-    # elif mode == "event":
-    #     _process_since_event(3263832, sys.argv[2])
-    # elif mode == "claims":
-    #     _process_claims()
-    # elif mode == "claim2":
-    #     _process_claims2()
-    # elif mode == "claim3":
-    #     _process_claims3()
-    # elif mode == "claim4":
-    #     _process_claims4()
-
+    elif mode == "event":
+        _process_since_event(3263832, sys.argv[2])
     else:
         raise Exception("Unspecified run mode")
-
     logger.info("Took %s", time() - ts)
 
 
@@ -127,15 +118,9 @@ if __name__ == "__main__":
         main("latest")
     else:
         if sys.argv[1] in (
-                "refresh",
                 "historical",
-                "txn_detail",
                 "latest",
                 "since",
                 "event",
-                "claims",
-                "claim2",
-                "claim3",
-                "claim4",
         ):
             main(sys.argv[1])
