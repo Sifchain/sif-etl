de464a9eb9ab68bf3027174df20cd2565c3d6cba
Shawn Xiao <daoqing@sifchain.finance>
2022-07-21 08:19:34 +1200

    bug fix

--- src/commands/event_record_continuous.py
+++ src/commands/event_record_continuous.py
@@ -3,7 +3,7 @@ from time import sleep
 from src.commands.latest_run import latest_run_command
 
 
-def event_record_continuous_command(testnet: int = 1):
+def event_record_continuous_command(testnet: int = None):
     while True:
         if testnet:
             latest_run_command(1)
